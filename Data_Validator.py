1bdeb59a85ce50c2a4a187fd0edb359fd485ba04
Silambarasan31 <silambarasaneee31@gmail.com>
2023-12-10 21:34:08 +0530

    Added soruce and target data to excel file

--- Data_Validator.py
+++ Data_Validator.py
@@ -18,7 +18,7 @@ try:
         df_source = pd.DataFrame(record,columns = column_names)
         # converting NaN to 'Empty'
         for col in df_source.columns:
-            df_source[col].replace({'':'Empty',np.nan:'Empty','None':'Empty'},inplace = True)
+            df_source[col].replace({'':'Empty',np.nan:'Empty'},inplace = True)
         # Detecting the encoding type
         with open('city_target.csv','rb') as file:
             encoding_type =  cd.detect(file.read())['encoding']
@@ -27,11 +27,16 @@ try:
         for col in df_target.columns:
             df_target[col].replace({'':'Empty',np.nan:'Empty','None':'Empty'},inplace = True)
         # Compare data
-        comparison_result = pd.merge(df_source, df_target, how='outer', indicator=True)#.loc[lambda x: x['_merge'] != 'both']
-        # Write the comparison result to an Excel file
-        comparison_result.to_excel('comparision.xlsx',index = False)
+        Comparing_data = {columns:(df_source[columns] == df_target[columns]) for columns in df_source.columns}
+        comparison_result = pd.DataFrame(Comparing_data)
+        # comparison_result = pd.merge(df_source, df_target, how='outer', indicator=True)#.loc[lambda x: x['_merge'] != 'both']
+        # Compared data in Excel sheet
+        with pd.ExcelWriter('Comparison.xlsx') as Writer:
+            df_source.to_excel(Writer, sheet_name = 'Source Data', index = False)
+            df_target.to_excel(Writer, sheet_name = 'Target Data', index = False)
+            comparison_result.to_excel(Writer, sheet_name = 'Comparison' ,index = False)
 
-        print("Comparison completed. Results saved to",'D:\VS Code programs\Data validator')
+        print("Comparison completed. Results saved to",'D:\VS Code programs\Data validator\Comparison.xlsx')
 except Error as e:
     print('while connecting to sql:',e)
 
